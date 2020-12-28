bea6c3ae4e29d5619aa6b647367f23e00cc78154
dagangfang <dagangfang@live.com>
2020-12-28 21:00:01 +0800

    submit

---
+++ daily/20201228#188/Solution.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <utility>
+#include <algorithm>
+#include <cmath>
+#include <sstream>
+#include <deque>
+#include <limits.h>
+#include <string.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(int k, vector<int>& prices) {
+
+    }
+};
+
+
+int main()
+{
+    Solution s = Solution();
+    // bool r = s.wordPattern("aaa", "aa aa aa aa");
+    // int r = s.monotoneIncreasingDigits(11023);
+    vector<int> in = vector<int>{1, 3, 2, 8, 4, 9};
+    // printf("结果：%d \n", s.maxProfit(in, 2));
+    return 1;
+}
