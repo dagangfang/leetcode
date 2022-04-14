07eae90c7cc45ef7387fd1c3c20f9827259669f2
dagangfang <dagangfang@live.com>
2022-04-14 21:30:02 +0800

    submit

--- daily/20220414#1672/Solution.cpp
+++ daily/20220414#1672/Solution.cpp
@@ -17,46 +17,29 @@
 
 using namespace std;
 
-
-class RandomizedSet {
+class Solution {
 public:
-    vector<int> vec;
-    RandomizedSet() {
-    }
-
-    bool insert(int val) {
-        for (auto& a : vec)
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int max = 0;
+        for (auto& account : accounts)
         {
-            if (a == val)
+            int temp = 0;
+            for (auto& num : account)
             {
-                return false;
+                temp += num;
             }
-        }
-        vec.push_back(val);
-        return true;
-    }
-
-    bool remove(int val) {
-        for (auto i = vec.begin();i != vec.end();++i)
-        {
-            if (*i == val)
+            if (temp > max)
             {
-                vec.erase(i);
-                return true;
+                max = temp;
             }
-        }
-        return false;
-    }
 
-    int getRandom() {
-        int index = rand() % vec.size();
-        return vec[index];
+        }
+        return max;
     }
 };
-
 int main()
 {
-    // Solution s = Solution();
+    Solution s = Solution();
     vector<string> s1 = { "gin", "zen", "gig", "msg" };
     // int b = s.uniqueMorseRepresentations(s1);
     // cout << "Solution:" << b << endl;
