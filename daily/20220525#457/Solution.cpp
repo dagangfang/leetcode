7f0199494a6dae3f34752c391fa182ee6ac69e8b
dagangfang <dagangfang@live.com>
2022-05-25 21:30:01 +0800

    submit

---
+++ daily/20220525#457/Solution.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <map>
+#include <utility>
+#include <algorithm>
+#include <cmath>
+#include <sstream>
+#include <deque>
+#include <limits.h>
+#include <string.h>
+#include <stack>
+#include <string>
+#include <list>
+#include <queue>
+
+using namespace std;
+
+class Solution {
+public:
+    int findSubstringInWraproundString(string p) {
+        int num = 0;
+        int n = p.size();
+        int l = 0;
+        int r = 0;
+        while (r < n)
+        {
+            
+
+
+        }
+    }
+};
+
+int main()
+{
+    Solution s = Solution();
+    //{3,4},{2,3},{1,2}
+    //{1,4},{2,3},{1,2}
+    // vector<vector<int>> nums = { {3,4},{2,3},{1,2} };
+    // // vector<int> ret = s.findRightInterval(nums);
+    // for (int i = 0;i < ret.size();++i)
+    // {
+    //     cout << "ret:" << ret[i] << endl;
+    // }
+
+    // int n = 8;
+    // vector<int> ret = s.diStringMatch("IDID");
+    // for (int i = 0;i < ret.size();++i)
+    // {
+    //     cout << ":" << ret[i] << endl;
+    // }
+    return 1;
+}
