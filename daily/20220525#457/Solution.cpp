358baea23523ad9c11ada4a8bd28eb3e55393115
dagangfang <dagangfang@live.com>
2022-05-26 21:30:01 +0800

    submit

--- daily/20220525#457/Solution.cpp
+++ daily/20220525#457/Solution.cpp
@@ -20,16 +20,35 @@ using namespace std;
 class Solution {
 public:
     int findSubstringInWraproundString(string p) {
-        int num = 0;
+        int nums[26] = { 0 };
+        int ret = 0;
         int n = p.size();
         int l = 0;
-        int r = 0;
-        while (r < n)
+        for (int i = 0;i < n;++i)
         {
-            
-
-
+            int t = i;
+            while (t == i || (t < n && (p[t] - p[t - 1] == 1 || p[t] - p[t - 1] == -25)))
+            {
+                ++t;
+            }
+            printf("while %d %d\n", i, t);
+            for (;i < t;++i)
+            {
+                int step = p[i] - 'a';
+                int sub = t - i;
+                if (nums[step] < sub)
+                {
+                    nums[step] = sub;
+                }
+            }
+            --i;
         }
+        for (int i = 0;i < 26;++i)
+        {
+            ret += nums[i];
+            printf("%d %d %d\n", i, nums[i], ret);
+        }
+        return ret;
     }
 };
 
@@ -39,12 +58,12 @@ int main()
     //{3,4},{2,3},{1,2}
     //{1,4},{2,3},{1,2}
     // vector<vector<int>> nums = { {3,4},{2,3},{1,2} };
-    // // vector<int> ret = s.findRightInterval(nums);
+    int ret = s.findSubstringInWraproundString("cac");
     // for (int i = 0;i < ret.size();++i)
     // {
     //     cout << "ret:" << ret[i] << endl;
     // }
-
+    cout << "ret:" << ret << endl;
     // int n = 8;
     // vector<int> ret = s.diStringMatch("IDID");
     // for (int i = 0;i < ret.size();++i)
