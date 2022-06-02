877df64bc3e14cb7246fe9819df64b33a4dfe2fb
dagangfang <dagangfang@live.com>
2022-06-02 21:30:01 +0800

    submit

---
+++ daily/20220602#450/Solution.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <map>
+#include <utility>
+#include <algorithm>
+#include <cmath>
+#include <sstream>
+#include <deque>
+#include <limits.h>
+#include <string.h>
+#include <stack>
+#include <string>
+#include <list>
+#include <queue>
+
+using namespace std;
+
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if (root == nullptr) {
+            return nullptr;
+        }
+        if (root->val > key) {
+            root->left = deleteNode(root->left, key);
+            return root;
+        }
+        if (root->val < key) {
+            root->right = deleteNode(root->right, key);
+            return root;
+        }
+        if (root->val == key) {
+            if (!root->left && !root->right) {
+                return nullptr;
+            }
+            if (!root->right) {
+                return root->left;
+            }
+            if (!root->left) {
+                return root->right;
+            }
+            TreeNode *successor = root->right;
+            while (successor->left) {
+                successor = successor->left;
+            }
+            root->right = deleteNode(root->right, successor->val);
+            successor->right = root->right;
+            successor->left = root->left;
+            return successor;
+        }
+        return root;
+    }
+};
+
+
+int main()
+{
+    Solution s = Solution();
+    //{3,4},{2,3},{1,2}
+    //{1,4},{2,3},{1,2}
+    // vector<vector<int>> nums = { {3,4},{2,3},{1,2} };
+    // string ret = s.removeOuterParentheses("(()())(())(()(()))");
+    // for (int i = 0;i < ret.size();++i)
+    // {
+    //     cout << "ret:" << ret[i] << endl;
+    // }
+    // cout << "ret:" << ret << endl;
+    // int n = 8;
+    // vector<int> ret = s.diStringMatch("IDID");
+    // for (int i = 0;i < ret.size();++i)
+    // {
+    //     cout << ":" << ret[i] << endl;
+    // }
+    return 1;
+}
