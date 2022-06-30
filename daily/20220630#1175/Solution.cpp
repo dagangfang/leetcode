9e1e6945fed82538763e4ce912c06804ac7763da
dagangfang <dagangfang@live.com>
2022-06-30 21:30:01 +0800

    submit

---
+++ daily/20220630#1175/Solution.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <map>
+#include <utility>
+#include <algorithm>
+#include <cmath>
+#include <sstream>
+#include <deque>
+#include <limits.h>
+#include <string.h>
+#include <stack>
+#include <string>
+#include <list>
+#include <queue>
+
+using namespace std;
+
+const int MOD = 1e9 + 7;
+
+class Solution {
+public:
+    int numPrimeArrangements(int n) {
+        int numPrimes = 0;
+        for (int i = 1; i <= n; i++) {
+            if (isPrime(i)) {
+                numPrimes++;
+            }
+        }
+        return (int) (factorial(numPrimes) * factorial(n - numPrimes) % MOD);
+    }
+
+    bool isPrime(int n) {
+        if (n == 1) {
+            return false;
+        }
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    long factorial(int n) {
+        long res = 1;
+        for (int i = 1; i <= n; i++) {
+            res *= i;
+            res %= MOD;
+        }
+        return res;
+    }
+};
+
+int main()
+{
+    Solution s = Solution();
+    // vector<int> arg = { 8,4,5,0,0,0,0,7 };
+    // s.duplicateZeros(arg);
+    // for (int i = 0;i < arg.size();++i)
+    // {
+    //     printf("%d \n", arg[i]);
+    // }
+    // return 1;
+}
