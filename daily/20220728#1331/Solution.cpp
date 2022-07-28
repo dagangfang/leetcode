2a7881dd216901f68fc8bc12fb571d0289817fc2
dagangfang <dagangfang@live.com>
2022-07-28 21:30:01 +0800

    submit

---
+++ daily/20220728#1331/Solution.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <map>
+#include <utility>
+#include <algorithm>
+#include <cmath>
+#include <sstream>
+#include <deque>
+#include <limits.h>
+#include <string.h>
+#include <stack>
+#include <string>
+#include <list>
+#include <queue>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> arrayRankTransform(vector<int>& arr) {
+        vector<int> sortedArr = arr;
+        sort(sortedArr.begin(), sortedArr.end());
+        unordered_map<int, int> ranks;
+        vector<int> ans(arr.size());
+        for (auto &a : sortedArr) {
+            if (!ranks.count(a)) {
+                ranks[a] = ranks.size() + 1;
+            }
+        }
+        for (int i = 0; i < arr.size(); i++) {
+            ans[i] = ranks[arr[i]];
+        }
+        return ans;
+    }
+};
+
+
+
+
+
+int main()
+{
+    // Solution s = Solution();
+    // vector<int> arg = { 8,4,5,0,0,0,0,7 };
+    // s.duplicateZeros(arg);
+    // for (int i = 0;i < arg.size();++i)
+    // {
+    //     printf("%d \n", arg[i]);
+    // }
+    // return 1;
+}
