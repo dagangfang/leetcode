da35e5e788e62f537bb714e0a2bd96e9500facca
dagangfang <dagangfang@live.com>
2022-08-09 21:30:01 +0800

    submit

---
+++ daily/20220809#1413/Solution.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <map>
+#include <utility>
+#include <algorithm>
+#include <cmath>
+#include <sstream>
+#include <deque>
+#include <limits.h>
+#include <string.h>
+#include <stack>
+#include <string>
+#include <list>
+#include <queue>
+#include<numeric>
+
+using namespace std;
+
+
+class Solution {
+public:
+    int minStartValue(vector<int>& nums) {
+        int min = 0;
+        int sum = min;
+        for (int i = 0;i < nums.size();++i)
+        {
+            sum += nums[i];
+            if (sum < min)
+            {
+                min = sum;
+            }
+        }
+        if (min >= 0) return 1;
+        return -min + 1;
+    }
+};
+
+
+
+
+
+
+int main()
+{
+    // Solution s = Solution();
+    // vector<int> arg = { 8,4,5,0,0,0,0,7 };
+    // s.duplicateZeros(arg);
+    // for (int i = 0;i < arg.size();++i)
+    // {
+    //     printf("%d \n", arg[i]);
+    // }
+    // return 1;
+}
