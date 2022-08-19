82b3ce4563421489e7a0c08564c1b3e521dc25bf
dagangfang <dagangfang@live.com>
2022-08-19 21:30:01 +0800

    submit

---
+++ daily/20220819#1450/Solution.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <map>
+#include <utility>
+#include <algorithm>
+#include <cmath>
+#include <sstream>
+#include <deque>
+#include <limits.h>
+#include <string.h>
+#include <stack>
+#include <string>
+#include <list>
+#include <queue>
+#include <numeric>
+
+using namespace std;
+
+
+class Solution {
+public:
+    int busyStudent(vector<int>& startTime, vector<int>& endTime, int queryTime) {
+        int sum = 0;
+        int n = startTime.size();
+        for (int i = 0;i < n;++i)
+        {
+            if (startTime[i] <= queryTime && queryTime <= endTime[i])
+                ++sum;
+        }
+        return sum;
+    }
+};
+
+int main()
+{
+    // Solution s = Solution();
+    // vector<int> arg = { 8,4,5,0,0,0,0,7 };
+    // s.duplicateZeros(arg);
+    // for (int i = 0;i < arg.size();++i)
+    // {
+    //     printf("%d \n", arg[i]);
+    // }
+    // return 1;
+}
+
