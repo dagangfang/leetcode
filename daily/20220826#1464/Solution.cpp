29ecc4dc4da8e0403717205c12446db4cbc9459c
dagangfang <dagangfang@live.com>
2022-08-26 21:30:01 +0800

    submit

---
+++ daily/20220826#1464/Solution.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <map>
+#include <utility>
+#include <algorithm>
+#include <cmath>
+#include <sstream>
+#include <deque>
+#include <limits.h>
+#include <string.h>
+#include <stack>
+#include <string>
+#include <list>
+#include <queue>
+#include <numeric>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        return max((nums[0] - 1) * (nums[1] - 1), (nums[n - 2] - 1) * (nums[n - 1] - 1));
+    }
+};
+
+
+int main()
+{
+    // Solution s = Solution();
+    // vector<int> arg = { 8,4,5,0,0,0,0,7 };
+    // s.duplicateZeros(arg);
+    // for (int i = 0;i < arg.size();++i)
+    // {
+    //     printf("%d \n", arg[i]);
+    // }
+    // return 1;
+}
+
