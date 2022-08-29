e3ca9bd181ff4711f4ad99397ecdd1c7c0269a94
dagangfang <dagangfang@live.com>
2022-08-29 21:30:01 +0800

    submit

---
+++ daily/20220827#1470/Solution.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <map>
+#include <utility>
+#include <algorithm>
+#include <cmath>
+#include <sstream>
+#include <deque>
+#include <limits.h>
+#include <string.h>
+#include <stack>
+#include <string>
+#include <list>
+#include <queue>
+#include <numeric>
+
+using namespace std;
+
+
+
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        vector<int> ret(2 * n);
+        for (int i = 0;i < n;++i)
+        {
+            ret[2 * i] = nums[i];
+            ret[2 * i + 1] = nums[i + n];
+        }
+        return ret;
+    }
+};
+
+
+int main()
+{
+    // Solution s = Solution();
+    // vector<int> arg = { 8,4,5,0,0,0,0,7 };
+    // s.duplicateZeros(arg);
+    // for (int i = 0;i < arg.size();++i)
+    // {
+    //     printf("%d \n", arg[i]);
+    // }
+    // return 1;
+}
+
