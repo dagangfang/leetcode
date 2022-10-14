55c4dfc9efc72996213a5c06c51029a92a389223
dagangfang <dagangfang@live.com>
2022-10-14 21:30:01 +0800

    submit

---
+++ daily/20221013#769/Solution.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <map>
+#include <utility>
+#include <algorithm>
+#include <cmath>
+#include <sstream>
+#include <deque>
+#include <limits.h>
+#include <string.h>
+#include <stack>
+#include <string>
+#include <list>
+#include <queue>
+#include <numeric>
+
+using namespace std;
+
+
+class Solution {
+public:
+    int maxChunksToSorted(vector<int>& arr) {
+        int n = arr.size();
+        int last = arr[0];
+        int ret = 1;
+        for (int i = 1;i < n;++i)
+        {
+            if (arr[i] < last)
+            {
+                
+            }
+        }
+    }
+};
+
+
+int main()
+{
+    // Solution s = Solution();
+    // vector<int> arg = { 8,4,5,0,0,0,0,7 };
+    // s.duplicateZeros(arg);
+    // for (int i = 0;i < arg.size();++i)
+    // {
+    //     printf("%d \n", arg[i]);
+    // }
+    // return 1;
+}
+
