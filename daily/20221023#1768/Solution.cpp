8a060717a8242155b068fc5f25e97a532adf1a1d
dagangfang <dagangfang@live.com>
2022-10-24 21:30:01 +0800

    submit

---
+++ daily/20221023#1768/Solution.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <map>
+#include <utility>
+#include <algorithm>
+#include <cmath>
+#include <sstream>
+#include <deque>
+#include <limits.h>
+#include <string.h>
+#include <stack>
+#include <string>
+#include <list>
+#include <queue>
+#include <numeric>
+
+using namespace std;
+
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        int n1 = word1.size();
+        int n2 = word2.size();
+        string ret;
+        int n = max(n1, n2);
+        for (int i = 0;i < n;++i)
+        {
+            if (i < n1)
+            {
+                ret += word1[i];
+            }
+            if (i < n2)
+            {
+                ret += word2[i];
+            }
+        }
+        return ret;
+    }
+};
+
+
+int main()
+{
+    // Solution s = Solution();
+    // vector<int> arg = { 8,4,5,0,0,0,0,7 };
+    // s.duplicateZeros(arg);
+    // for (int i = 0;i < arg.size();++i)
+    // {
+    //     printf("%d \n", arg[i]);
+    // }
+    // return 1;
+}
+
---
+++ daily/20221024#915/Solution.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <map>
+#include <utility>
+#include <algorithm>
+#include <cmath>
+#include <sstream>
+#include <deque>
+#include <limits.h>
+#include <string.h>
+#include <stack>
+#include <string>
+#include <list>
+#include <queue>
+#include <numeric>
+
+using namespace std;
+
+class Solution {
+public:
+    int partitionDisjoint(vector<int>& nums) {
+        int n = nums.size();
+        int leftMax = nums[0], leftPos = 0, curMax = nums[0];
+        for (int i = 1; i < n - 1; i++) {
+            curMax = max(curMax, nums[i]);
+            if (nums[i] < leftMax) {
+                leftMax = curMax;
+                leftPos = i;
+            }
+        }
+        return leftPos + 1;
+    }
+};
+
+
+int main()
+{
+    // Solution s = Solution();
+    // vector<int> arg = { 8,4,5,0,0,0,0,7 };
+    // s.duplicateZeros(arg);
+    // for (int i = 0;i < arg.size();++i)
+    // {
+    //     printf("%d \n", arg[i]);
+    // }
+    // return 1;
+}
+
